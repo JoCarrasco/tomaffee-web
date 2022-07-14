4f0ec1a5fb44cc058051ce39a0dd9e979ceba7b4
JoCarrasco <jogregcarrasco@gmail.com>
2022-07-14 18:22:43 -0400

    revert: Previous changes code structure

--- src/pages/Home/Home.page.tsx
+++ src/pages/Home/Home.page.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { TimeEntryService, TimeEntryListComponent } from "../../core";
 import { useTimeEntries } from '../../core/hooks';
+import { ITimeEntry } from '../../core/models/api';
 
 function HomePage() {
   const entries = useTimeEntries();
@@ -8,6 +9,22 @@ function HomePage() {
   useEffect(() => {
  
   }, [entries]);
+
+  function handleStop(timeEntryId: string) {
+    TimeEntryService.stopTimeEntry(timeEntryId);
+  }
+
+  function handleRemove(timeEntryId: string) {
+    TimeEntryService.removeTimeEntry(timeEntryId);
+  }
+
+  function handleContinue(timeEntryId: string) {
+    TimeEntryService.continueTimeEntry(timeEntryId);
+  }
+
+  function handleChange(timeEntryId: string, change: Partial<ITimeEntry>) {
+    TimeEntryService.updateTimeEntry(timeEntryId, change);
+  }
 
   return (
     <div className="App">
@@ -18,10 +35,10 @@ function HomePage() {
       {/* <TimeEntryButtonComponent /> */}
       <TimeEntryListComponent
         entries={entries}
-        onContinue={TimeEntryService.continueTimeEntry}
-        onStop={TimeEntryService.stopTimeEntry}
-        onRemove={TimeEntryService.removeTimeEntry}
-        onChange={TimeEntryService.updateTimeEntry}
+        onContinue={handleContinue}
+        onStop={handleStop}
+        onRemove={handleRemove}
+        onChange={handleChange}
       />
     </div>
   );
