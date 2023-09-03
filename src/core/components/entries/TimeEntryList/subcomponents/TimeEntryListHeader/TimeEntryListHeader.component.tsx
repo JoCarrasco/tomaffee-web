0faef8ebae7c0c497cb22719e9157735a443809e
Jo Carrasco <jogregcarrasco@gmail.com>
2023-09-02 20:22:16 -0400

    fix: remove unused lines of code

--- src/core/components/entries/TimeEntryList/subcomponents/TimeEntryListHeader/TimeEntryListHeader.component.tsx
+++ src/core/components/entries/TimeEntryList/subcomponents/TimeEntryListHeader/TimeEntryListHeader.component.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { DateHelper } from '../../../../../classes';
 import './TimeEntryListHeader.styles.scss';
 
@@ -8,9 +7,8 @@ interface ITimeEntryListHeaderComponentProps {
 
 export const TimeEntryListHeaderComponent = (props: ITimeEntryListHeaderComponentProps) => {
   return (
-      <div className="time-entry-list-header">
-        <span>{DateHelper.toFriendlyDate(props.date)}</span>
-        {/* <button onClick={() => setBulkEdit(!bulkEdit)}>Bulk Edit</button> */}
-      </div>
+    <div className="time-entry-list-header">
+      <span>{DateHelper.toFriendlyDate(props.date)}</span>
+    </div>
   );
 }
